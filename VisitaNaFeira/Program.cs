e0dac9ad6b3199707c588c901021eb1efa6431d2
avilalopes <82402280+avilalopes@users.noreply.github.com>
2021-07-23 13:35:54 -0300

    Add files via upload

---
+++ VisitaNaFeira/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class minhaClasse{
+
+    static void Main(string[] args) { 
+
+            int a, b, x;
+            Console.WriteLine("Informe a quantidade de pimentões amarelos e vermelhos respectivamente:");
+            string [] quantidade = Console.ReadLine().Split(' ');
+            a = Convert.ToInt32(quantidade[0]);
+            b = Convert.ToInt32(quantidade[1]);
+
+            x = (a) + (b); //complete o código de acordo com o cálculo esperado
+
+            Console.WriteLine("X = {0}", x);
+    }
+}
